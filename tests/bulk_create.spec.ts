8cb3e2e2d298d65f79afeb6a283fe0ea358d2c74
NauanyC <nauanycc@outlook.com.br>
2023-05-16 20:09:15 -0300

    test/refactor: use in memory db for tests

--- tests/bulk_create.spec.ts
+++ tests/bulk_create.spec.ts
@@ -10,13 +10,7 @@ describe("Bulk Create", () => {
   beforeAll(async () => {
     await extendSequelize(Sequelize);
 
-    mockedSequelize = new Sequelize({
-      dialect: "postgres",
-      host: process.env.DB_HOST ?? "localhost",
-      port: Number(process.env.DB_PORT) ?? 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+    mockedSequelize = new Sequelize("sqlite::memory:", {
       logging: false,
     });
   });
